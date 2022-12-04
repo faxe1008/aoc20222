166bc18d791fc06a397530be59fa23661e95462c
faxe1008 <fabianblatz@gmail.com>
2022-12-04 17:20:44 +0100

    Solution day1, riddle2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::fs;
 
-fn riddle_part_one(file_path: &String) {
+fn riddle_part_one(file_path: &String) -> Vec<u32> {
     let text = fs::read_to_string(file_path).expect("Error reading file");
 
     let calories_per_elf: Vec<u32> = text
@@ -25,9 +25,16 @@ fn riddle_part_one(file_path: &String) {
 
     let highest_caloric_count = calories_per_elf.iter().max_by_key(|p| p.clone()).unwrap();
     println!("Highest caloric count: {}", highest_caloric_count);
+    calories_per_elf
 }
 
-fn riddle_part_two(file_path: &String) {}
+fn riddle_part_two(file_path: &String) {
+    let mut calories_per_elf = riddle_part_one(file_path);
+    calories_per_elf.sort_by(|a, b| b.cmp(a));
+
+    let top_three_total_cal = calories_per_elf[0..3].iter().sum::<u32>();
+    println!("Top three elfes are carying: {:?}", top_three_total_cal);
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -41,8 +48,14 @@ fn main() {
         .expect("Error parsing riddle num");
 
     match riddle_num {
-        1 => riddle_part_one(args.get(2).unwrap()),
-        2 => riddle_part_two(args.get(2).unwrap()),
-        _ => panic!("Unknown riddle part number"),
-    }
+        1 => {
+            riddle_part_one(args.get(2).unwrap());
+        }
+        2 => {
+            riddle_part_two(args.get(2).unwrap());
+        }
+        _ => {
+            panic!("Unknown riddle part number");
+        }
+    };
 }
