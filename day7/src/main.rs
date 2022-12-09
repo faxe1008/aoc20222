52eac0f532cb184b9201ee904221ba69f3763223
faxe1008 <fabianblatz@gmail.com>
2022-12-09 15:57:13 +0100

    Solution day7, riddle1

---
+++ day7/src/main.rs
@@ -0,0 +1,161 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::ops::Index;
+use std::fs;
+use std::env;
+
+
+#[derive(Debug)]
+struct DirectoryInfo {
+    name: String,
+    files: Vec<FileInfo>,
+    directories: Vec<DirectoryInfo>
+}
+
+#[derive(Debug)]
+struct FileInfo {
+    name: String, 
+    size: usize
+}
+
+#[derive(Debug)]
+struct FileSystem {
+    root: DirectoryInfo
+}
+
+type FileSystemPath = Vec<String>;
+
+impl DirectoryInfo {
+    fn recursive_size(&self) -> usize {
+        let mut total_size = self.files.iter().map(|y| y.size).sum();
+        for directory in self.directories.iter() {
+            total_size += directory.recursive_size();
+        }
+        return total_size;
+    }
+}
+
+impl FileSystem {
+    fn new() -> Self {
+        Self { 
+            root: DirectoryInfo { name: "<root>".to_string(),  files: Vec::new(), directories: Vec::new()}
+        }
+    }
+
+    fn mut_directory_by_path(&mut self, path: &FileSystemPath) -> Option<&mut DirectoryInfo> {
+        if path.is_empty() {
+            return Some(&mut self.root); 
+        }
+
+        let mut current_directory = &mut self.root;
+        for path_segment in path.iter() {
+
+           let next_directory = current_directory.directories.iter_mut().find(|y| y.name == *path_segment);
+           current_directory = next_directory?;
+        }   
+        return Some(current_directory);
+    }
+
+    fn insert(&mut self, path: &FileSystemPath, files: &mut Vec<FileInfo>, directories: &mut Vec<DirectoryInfo>) {
+        if let Some( directory) = self.mut_directory_by_path(path) {
+            directory.directories.append(directories);
+            directory.files.append(files);
+        }
+    }
+}
+
+fn riddle_part_one(file_path: &String)  {
+    let text = fs::read_to_string(file_path).expect("Error reading file");
+    let command_blocks : Vec<&str> = text.split("\n$").map(|y| y.trim()).collect();
+
+    let mut cwd = FileSystemPath::new();
+    let mut fs = FileSystem::new();
+
+    let mut all_paths : HashSet<FileSystemPath>= HashSet::new();
+
+    for command in command_blocks {
+
+        if command.starts_with("cd ") {
+            let destination = &command[3..];
+            if destination == ".." {
+                cwd.pop();
+            }else{
+                cwd.push(destination.to_string());
+            }
+        }
+
+        if command.starts_with("ls") {
+            let response_lines : Vec<&str>= command.split("\n").map(|y| y.trim()).skip(1).collect();
+
+            let mut dirs : Vec<DirectoryInfo> = Vec::new();
+            let mut files : Vec<FileInfo> = Vec::new();
+            for response in response_lines {
+                if response.starts_with("dir ") {
+                    let dir_name = &response[4..];
+                    let dir = DirectoryInfo { name: dir_name.to_string(), directories: Vec::new(), files: Vec::new() };
+                    dirs.push(dir);
+                    
+                    let mut new_dir = cwd.clone();
+                    new_dir.push(dir_name.to_string());
+                    all_paths.insert(new_dir);
+                }else {
+                    let file_result_parts :Vec<&str>= response.split(" ").collect();
+                    if file_result_parts.len() != 2 {
+                        continue;
+                    }
+                    let file_info = FileInfo {
+                        name: file_result_parts[1].to_string(),
+                        size: file_result_parts[0].parse::<usize>().expect("Error parsing size")
+                    };
+                    files.push(file_info);
+                }
+            }
+            fs.insert(&cwd, &mut files, &mut dirs);
+        } 
+    }
+
+    //println!("DUMPING FS: {:?}", fs);
+
+    let mut path_size_map : HashMap<FileSystemPath, usize> = HashMap::new();
+    for path in all_paths.iter() {
+        if let Some(directory)  = fs.mut_directory_by_path(path) {
+            let size = directory.recursive_size();
+            path_size_map.insert(path.clone(), size);
+        }
+    }
+
+    let sum_dirs_beneath_size : usize = path_size_map.iter_mut()
+                                        .filter(|entry| *entry.1 < 100000)
+                                        .map(|entry| *entry.1).sum();
+
+    println!("Sum of all dir size matching criteria: {}", sum_dirs_beneath_size);
+}
+
+fn riddle_part_two(file_path: &String) {
+    let text = fs::read_to_string(file_path).expect("Error reading file");
+
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        panic!("Provide the input text file!");
+    }
+    let riddle_num: u32 = args
+        .get(1)
+        .unwrap()
+        .parse()
+        .expect("Error parsing riddle num");
+
+    match riddle_num {
+        1 => {
+            riddle_part_one(args.get(2).unwrap());
+        }
+        2 => {
+            riddle_part_two(args.get(2).unwrap());
+        }
+        _ => {
+            panic!("Unknown riddle part number");
+        }
+    };
+}
